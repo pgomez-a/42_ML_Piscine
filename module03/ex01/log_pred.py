92134d092ba6dabe89fa048b358f35847dcc0208
pgomez-a <pablogomezalvarez02@gmail.com>
2022-05-30 16:44:31 +0200

    Upload module03 ex01

---
+++ module03/ex01/log_pred.py
@@ -0,0 +1,24 @@
+import numpy as np
+import math
+
+def sigmoid_(x):
+    """
+    Computes the sigmoid of a vector.
+    """
+    if not type(x) == np.ndarray or x.size == 0:
+        return
+    return 1 / (1 + math.e**-x)
+
+def logistic_predict_(x, theta):
+    """
+    Computes the vector of prediction y_hat from two non-emtpy
+    numpy.ndarray.
+    """
+    if not type(x) == np.ndarray or x.size == 0:
+        return
+    if not type(theta) == np.ndarray or theta.size == 0:
+        return
+    if x.shape[1] != theta.shape[0] - 1:
+        return
+    x = np.insert(x, 0, [1] * x.shape[0], 1)
+    return sigmoid_(np.matmul(x, theta))
