cc64507aa6b7e58d5fad3e782e618e87350e93b2
pgomez-a <pablogomezalvarez02@gmail.com>
2022-06-19 18:27:25 +0200

    Update module03 ex08

--- module03/ex08/other_metrics.py
+++ module03/ex08/other_metrics.py
@@ -4,57 +4,65 @@ def accuracy_score_(y, y_hat):
     """
     Computes the accuracy score.
     """
-    try:
-        true = 0
-        for pos in range(len(y)):
-            if y[pos] == y_hat[pos]:
-                true += 1
-        return true / y.size
-    except:
+    if not type(y) == np.ndarray or not type(y_hat) == np.ndarray:
         return
+    if y.shape != y_hat.shape:
+        return
+    true_value = 0
+    for pos in range(len(y)):
+        if y[pos] == y_hat[pos]:
+            true_value += 1
+    return true_value / y.size
+
 
 def precision_score_(y, y_hat, pos_label = 1):
     """
     Computes the precision score.
     """
-    try:
-        true_pos = 0
-        false_pos = 0
-        for pos in range(len(y)):
-            if y[pos] == y_hat[pos] and y_hat[pos] == pos_label:
-                true_pos += 1
-            if y[pos] != y_hat[pos] and y_hat[pos] == pos_label:
-                false_pos += 1
-        output = true_pos / (true_pos + false_pos)
-        return output
-    except:
+    if not type(y) == np.ndarray or not type(y_hat) == np.ndarray:
         return
+    if y.shape != y_hat.shape:
+        return
+    if not type(pos_label) == int and not type(pos_label) == str:
+        return
+    true_positive = 0
+    false_positive = 0
+    for pos in range(len(y)):
+        if y[pos] == pos_label and y_hat[pos] == pos_label:
+            true_positive += 1
+        if y[pos] != pos_label and y_hat[pos] == pos_label:
+            false_positive += 1
+    return true_positive / (true_positive + false_positive)
 
 def recall_score_(y, y_hat, pos_label = 1):
     """
     Computes the recall score.
     """
-    try:
-        true_pos = 0
-        false_neg = 0
-        for pos in range(len(y)):
-            if y[pos] == y_hat[pos] and y_hat[pos] == pos_label:
-                true_pos += 1
-            if y[pos] != y_hat[pos] and y_hat[pos] != pos_label:
-                false_neg += 1
-        output = true_pos / (true_pos + false_neg)
-        return output
-    except:
+    if not type(y) == np.ndarray or not type(y_hat) == np.ndarray:
         return
+    if y.shape != y_hat.shape:
+        return
+    if not type(pos_label) == int and not type(pos_label) == str:
+        return
+    true_positive = 0
+    false_negative = 0
+    for pos in range(len(y)):
+        if y[pos] == pos_label and y_hat[pos] == pos_label:
+            true_positive += 1
+        if y[pos] == pos_label and y_hat[pos] != pos_label:
+            false_negative += 1
+    return true_positive / (true_positive + false_negative)
 
 def f1_score_(y, y_hat, pos_label = 1):
     """
-    Computes the f1 score.
+    Compute the f1 score.
     """
-    try:
-        precision = precision_score_(y, y_hat, pos_label)
-        recall = recall_score_(y, y_hat, pos_label)
-        output = (2 * precision * recall) / (precision + recall)
-        return output
-    except:
+    if not type(y) == np.ndarray or not type(y_hat) == np.ndarray:
         return
+    if y.shape != y_hat.shape:
+        return
+    if not type(pos_label) == int and not type(pos_label) == str:
+        return
+    precision = precision_score_(y, y_hat, pos_label)
+    recall = recall_score_(y, y_hat, pos_label)
+    return 2 * (precision * recall) / (precision + recall)
